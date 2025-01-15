e98c334bbe5f18fac4c873e8a87535fcb2fa365b
teguhwaluyojati14 <teguhwaluyojati14@gmail.com>
2025-01-15 22:56:45 +0700

    Fixing Auth:Sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,30 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
 
 Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/login', [LoginController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return response()->json([
+            'status' => true,
+            'user' => $request->user(),
+        ]);
+    });
+
+    Route::get('/protected-data', function () {
+        return response()->json([
+            'status' => true,
+            'data' => 'This is protected data.',
+        ]);
+    });
+});
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 // Route::middleware('auth:sanctum')->group(function () {
 //     Route::post('/user/update', [UserController::class, 'update']);
